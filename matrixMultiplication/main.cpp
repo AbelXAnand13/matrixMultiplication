e49a67a8bf4106965a782ab18d61f50e0c192841
Abel Anand <Abelanand13@gmail.com>
2022-02-08 10:45:23 -0800

    commit2

--- matrixMultiplication/main.cpp
+++ matrixMultiplication/main.cpp
@@ -15,15 +15,29 @@ int main() {
         {55, 64, 58}
     };
     vector<vector<int>> c;
-
+//    ┌                  ┐
+//    │ a00  a01  a02    │
+//A = │ a10  a11  a12    │
+//    │ a20  a21  a22    │
+//    └                  ┘
+//    ┌                  ┐
+//    │ b00  b01  b02    │
+//B = │ b10  b11  b12    │
+//    │ b20  b21  b22    │
+//    └                  ┘
+//    ┌                                                                                                                   ┐
+//    │ a00 * b00 + a01 * b10 + a02 * b20     a00 * b01 + a01 * b11 + a02 * b21      a00 * b02 + a01 * b12 + a02 * b22    │
+//C = │ a10 * b00 + a11 * b10 + a12 * b20     a10 * b01 + a11 * b11 + a12 * b21      a10 * b02 + a11 * b12 + a12 * b22    │
+//    │ a20 * b00 + a21 * b10 + a22 * b20     a20 * b01 + a21 * b11 + a22 * b21      a20 * b02 + a21 * b12 + a22 * b22    │
+//    └                                                                                                                   ┘
     for (int i = 0; i < a.size(); i++) {
         vector<int> temp;//temporary vector thats gonna dump into c
-        int num = 0;
-        for (int j = 0; j < a[i].size(); j++) {
-            temp.push_back(a[i][num]*b[num][0]);
-            num++;
+        for (int j = 0; j < a[i].size();j++) {
+            for (int x = 0; x < temp.size(); x++) {
+                temp.push_back(a[i][j]*b[i][j] + a[i][j]*b[i][j] + a[i][j]*b[i][j]);
+            }
+            c.push_back(temp);
         }
-        c.push_back(temp);
     }
     
     for (int i = 0; i < c.size(); i++) {
